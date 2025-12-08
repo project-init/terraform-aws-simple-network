d0fa21caf996b56ac0aa1ead57bb500c6d333c1f
drush211 <20804720+drush211@users.noreply.github.com>
2025-12-08 11:39:24 -0600

    Update to the latest cloudposse subnets.

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "vpc" {
 
 module "subnets" {
   source  = "cloudposse/dynamic-subnets/aws"
-  version = "2.4.2"
+  version = "3.0.1"
   context = module.label.context
 
   vpc_id                  = module.vpc.vpc_id
